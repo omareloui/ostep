e3aa17e953f5914af21ab1001f20e4205701316c
Omar Eloui <contact@omareloui.com>
2026-03-18 00:54:06 +0200

    feat(chapter_1): add cpu, mem, and common files

---
+++ ch1/mem.c
@@ -0,0 +1,22 @@
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  int *p = malloc(sizeof(int));
+  assert(p != NULL);
+
+  printf("(%d) address pointed to by p: %p\n", getpid(), p);
+
+  *p = 0;
+
+  while (1) {
+    Spin(1);
+    *p = *p + 1;
+    printf("(%d) p: %d\n", getpid(), *p);
+  }
+
+  return 0;
+}
